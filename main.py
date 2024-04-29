ef192b47f40ba31168b7ac6b71a50c08dbcaf18d
zoeys-freind <129983142+zoeys-freind@users.noreply.github.com>
2024-04-29 13:18:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def tenorsearch(search_term):
     if r.status_code == 200:
         # load the GIFs using the urls for the smaller GIF sizes
         top_8gifs = r.json()
-        return send_file(BytesIO(req.get(top_8gifs["results"][0]["media_formats"]["mediumgif"]["url"]).content), as_attachment=True, attachment_filename=search_term+".gif")
+        return send_file(BytesIO(req.get(top_8gifs["results"][0]["media_formats"]["mediumgif"]["url"]).content), as_attachment=True, mimetype="image/gif")
     else:
         abort(r.status_code)
 
